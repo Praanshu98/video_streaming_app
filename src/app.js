da4a16b0996adcc2368926f0b5318d5a8c330288
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-22 01:59:12 +0530

    feat: add health check route and controller for API health verification, enhancing system monitoring capabilities

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -48,5 +49,6 @@ app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
